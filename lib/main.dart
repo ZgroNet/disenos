ebed422ee5895f13b0800cfac52fad7ff9724d53
ZgroNet <jmartidomenech@gmail.com>
2022-07-04 22:12:19 +0200

    Separando HomeScreen en un Widget (Video 141)

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Center(
-        child: Text('Hola Mundo'),
-      ),
+      home:  _HomeScreen(),
+    );
+  }
+}
+
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+          Text('Hola Mundo'),
+          Text('Hola Mundo'),
+      ],
     );
   }
 }
