b91a34af25b4a8ec31a13f002bb2585296d3610e
ZgroNet <jmartidomenech@gmail.com>
2022-07-05 09:19:32 +0200

    Añadido widget TitleScreen, siguiendo tutorial flutter.dev (Video 143)

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
 class BasicDesignScreen extends StatelessWidget {
-   
   const BasicDesignScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Image(image: AssetImage('assets/lake.jpg'), width: 600, height: 240, fit: BoxFit.cover)
+    return Scaffold(
+        body: Column(
+      children: const [
+        // Imagen
+        Image(
+            image: AssetImage('assets/lake.jpg'),
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover),
+
+        // Título
+        TitleSection()
+      ],
+    ));
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded (
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:  [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+        ],
       ),
     );
   }
