0c08534f6aea832dec54e048f880c7cfb951a81a
ZgroNet <jmartidomenech@gmail.com>
2022-07-06 17:41:37 +0200

    Background finalizado (Video 148)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:disenos/widgets/background.dart';
+
 class HomeScreen extends StatelessWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
+    return Scaffold(
+      body: Stack(
+        children: const [
+          // Fondo
+          Background()
+        ],
+      )
     );
   }
 }
